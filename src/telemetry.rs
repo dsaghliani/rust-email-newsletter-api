fdd5f000fa3df0abdaa90e30bfaa8b282c7c1bcc
Demetre Saghliani <saghliani.d@gmail.com>
2023-03-13 15:14:23 +0400

    Switch to `tracing-bunyan-formatter`.

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,13 +1,17 @@
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{
-    fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter,
+    layer::SubscriberExt, util::SubscriberInitExt, EnvFilter,
 };
 
 pub fn init_subscriber() {
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| "zero2prod=DEBUG,tower_http=DEBUG".into());
+    let formatting_layer =
+        BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
     tracing_subscriber::registry()
-        .with(
-            EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "zero2prod=DEBUG,tower_http=DEBUG".into()),
-        )
-        .with(fmt::layer().event_format(fmt::format().pretty()))
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
         .init();
 }
