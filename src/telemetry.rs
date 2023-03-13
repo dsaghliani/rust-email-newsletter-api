461acc5a3edb21db033d6e4d946528ff7aca5155
Demetre Saghliani <saghliani.d@gmail.com>
2023-03-13 15:00:28 +0400

    Better format `RUST_LOG` variable for `EnvFilter`.

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -6,7 +6,7 @@ pub fn init_subscriber() {
     tracing_subscriber::registry()
         .with(
             EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "zero2prod=debug,tower_http=debug".into()),
+                .unwrap_or_else(|_| "zero2prod=DEBUG,tower_http=DEBUG".into()),
         )
         .with(fmt::layer())
         .init();
