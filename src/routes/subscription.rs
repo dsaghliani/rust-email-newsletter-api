93216653bc9ad870ab7f49d9ad9e3b96b32a544f
Demetre Saghliani <saghliani.d@gmail.com>
2023-03-13 15:24:10 +0400

    Extract subscriber insertion into its own function.

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -3,7 +3,7 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Form};
 use serde::Deserialize;
 use sqlx::PgPool;
-use tracing::{error, info_span, Instrument};
+use tracing::error;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -25,9 +25,22 @@ pub async fn subscribe(
     State(connection_pool): State<PgPool>,
     Form(subscription_data): Form<SubscriptionData>,
 ) -> impl IntoResponse {
-    let query_span = info_span!("Saving new subscriber details in the database");
+    if (insert_subscriber(&connection_pool, &subscription_data).await).is_ok() {
+        StatusCode::OK
+    } else {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
 
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip_all
+)]
+async fn insert_subscriber(
+    connection_pool: &PgPool,
+    subscription_data: &SubscriptionData,
+) -> sqlx::Result<()> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name)
         VALUES ($1, $2, $3)
@@ -36,14 +49,14 @@ pub async fn subscribe(
         subscription_data.email,
         subscription_data.name
     )
-    .execute(&connection_pool)
-    .instrument(query_span)
+    .execute(connection_pool)
     .await
-    {
-        Ok(_) => StatusCode::OK,
-        Err(error) => {
-            error!("Failed to execute query: {error}");
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
+    // In the future, when it has stabilized, this can be replaced with
+    // `Result::inspect_err`.
+    .map_err(|error| {
+        error!("Faild to execute query: {error:?}");
+        error
+    })?;
+
+    Ok(())
 }
