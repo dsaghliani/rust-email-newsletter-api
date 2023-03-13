164eb9a5bc3ecaf061db9da4a03c4b1311a0a0a5
Demetre Saghliani <saghliani.d@gmail.com>
2023-03-13 14:59:04 +0400

    Instrument `routes::subscription::subscribe`.

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -3,7 +3,7 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Form};
 use serde::Deserialize;
 use sqlx::PgPool;
-use tracing::error;
+use tracing::{error, info_span, Instrument};
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -12,11 +12,21 @@ pub struct SubscriptionData {
     email: String,
 }
 
-#[axum::debug_handler]
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(connection_pool, subscription_data),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %subscription_data.email,
+        subscriber_name = %subscription_data.name,
+    )
+)]
 pub async fn subscribe(
     State(connection_pool): State<PgPool>,
     Form(subscription_data): Form<SubscriptionData>,
 ) -> impl IntoResponse {
+    let query_span = info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name)
@@ -27,6 +37,7 @@ pub async fn subscribe(
         subscription_data.name
     )
     .execute(&connection_pool)
+    .instrument(query_span)
     .await
     {
         Ok(_) => StatusCode::OK,
